e8c813bb7de9bbbb0d4dc1b31c2a30c988296c3b
Marlalei <marlaleidy01244@gmail.com>
2025-02-23 19:42:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import joblib
-import numpy as np
-from sklearn import * 
+from sklearn.feature_extraction.text import TfidVectorizer
+from sklearn.preprocessing import LabelEncoder
 app = Flask(__name__)
 
 # Load the model, vectorizer, and label encoder
